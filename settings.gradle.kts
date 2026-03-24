f42d7df644de544b64223bd2e6f95d0ba415da2b
Daz DeBoer <daz@gradle.com>
2026-03-24 08:55:01 -0600

    Prepare for testing configuration-cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-    id("com.gradle.develocity") version "4.2.1"
+    id("com.gradle.develocity") version "4.4.2"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
 }
 
@@ -10,7 +10,6 @@ develocity {
         uploadInBackground = false
     }
 }
-
 
 rootProject.name = "simple-test-project"
 include("app", "list", "utilities")
