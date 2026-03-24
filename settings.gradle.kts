d6d3d374c4901f419c90d79d0f3f1dc6a2ab87c9
Daz DeBoer <daz@gradle.com>
2026-03-24 08:55:01 -0600

    Workflow to build and publish scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-    id("com.gradle.develocity") version "4.2.1"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
 }
-
-develocity {
-    server = "https://ge.solutions-team.gradle.com"
-    buildScan {
-        uploadInBackground = false
-    }
-}
-
 
 rootProject.name = "simple-test-project"
 include("app", "list", "utilities")
